1a9fa8c2aff0c1bf7f6f9aba3108b903746376c2
pfeilda <dp@api-studio.de>
2018-03-28 07:49:35 +0200

    Development

---
+++ Classes/Utility/BackendUtility.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace DanielPfeil\Samlauthentication\Utility;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+//TODO maybe hooks at the start and the end of the ItemProcFunc
+class BackendUtility
+{
+    private $tableMappingTableName = 'tx_samlauthentication_domain_model_tablemapping';
+
+    public function getTables(array &$params) {
+        foreach ($this->getConnections() as $connection){
+            $tables = $connection->getSchemaManager()->listTableNames();
+            foreach ($tables as $table){
+                $params["items"][] = [$table, $table];
+            }
+        }
+    }
+
+    public function getFields(array &$params) {
+        $queryBuilderTableMapping = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($this->tableMappingTableName);
+        //TODO export to a factory method
+        $tableRecord = $queryBuilderTableMapping->select('table')
+            ->from($this->tableMappingTableName)
+            ->where(
+                $queryBuilderTableMapping->expr()->eq(
+                    'uid',
+                    $queryBuilderTableMapping->createNamedParameter(
+                        $params['row']['table'],
+                        \PDO::PARAM_INT
+                    )
+                )
+            )
+            ->execute()
+            ->fetch();
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $connection = $connectionPool->getConnectionForTable($this->tableMappingTableName);
+        $fields = $connection->query(
+            "DESC `" .
+            $connection->getDatabase() .
+            "`.`" .
+            $tableRecord["table"]
+            ."`;"
+        )->fetchAll();
+
+        foreach ($fields as $field){
+            $params["items"][] = [
+                $field['Field'],
+                $field['Field']
+            ];
+        }
+    }
+
+    private function getConnections(): array{
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $connectionNames = $connectionPool->getConnectionNames();
+        $connections = [];
+        foreach ($connectionNames as $connectionName) {
+            $connections[] = $connectionPool->getConnectionByName($connectionName);
+        }
+        return $connections;
+    }
+}
