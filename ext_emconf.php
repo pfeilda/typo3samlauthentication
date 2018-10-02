d74405ef23e3353676ac52b442a8799ce6e24cb9
Daniel Pfeil <dp@api-studio.de>
2018-10-02 09:13:45 +0200

    updated version tag and licence information

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,14 +20,14 @@
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'SAML Authentication',
     'description' => 'This extension adds a authentication method for SAML with different service provider.',
-    'version' => '0.0.1',
+    'version' => '1.1.0',
     'category' => 'services',
     'constraints' => array(
         'depends' => array(
             'typo3' => '8.7.0-9.0.0'
         )
     ),
-    'state' => 'alpha',
+    'state' => 'stable',
     'author' => 'Daniel Pfeil',
     'author_email' => 'daniel.pfeil@itpfeil.de'
 );
