a97de0e128ae1415d8de1f0b9e8eb3434dfb037d
Daniel Pfeil <d.pfeil@api-studio.de>
2019-05-20 21:55:10 +0200

    raised and fixed version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'SAML Authentication',
     'description' => 'This extension adds a authentication method for SAML with different service provider.',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'category' => 'services',
     'constraints' => array(
         'depends' => array(
